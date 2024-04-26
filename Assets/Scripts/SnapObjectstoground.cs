0798b9d2615d5f7f1a6387bcd07af58285078d59
Sathvikreddyv <50298451+Sathvikreddyv@users.noreply.github.com>
2024-04-26 17:25:14 -0400

    changes prefabs made is kinematic

---
+++ Assets/Scripts/SnapObjectstoground.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
+
+public class SnapObjectstoground : MonoBehaviour
+{
+    public InputActionReference PrimaryButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!PrimaryButton.action.triggered)
+        {
+            transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+        }
+    }
+}
