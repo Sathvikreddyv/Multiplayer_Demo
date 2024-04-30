ac3928b8c633d522e3b93d50ba24bb85ac8f097d
Sathvikreddyv <50298451+Sathvikreddyv@users.noreply.github.com>
2024-04-30 18:38:58 -0400

    Changes_ObjectManipulationXR

--- Assets/Scripts/ObjectManipulation.cs
+++ Assets/Scripts/ObjectManipulation.cs
@@ -33,24 +33,24 @@ public class ObjectManipulation : MonoBehaviourPunCallbacks, IPunObservable
     {
         //XR
         #region handling rotation and ray interaction through XR input(trigger Left/Right)
-        if (LeftTriggerButtonAction != null && LeftTriggerButtonAction.action != null && LeftTriggerButtonAction.action.triggered)
-        {
-            Ray ray = new Ray(rayInteractor.transform.position, rayInteractor.transform.forward);
-            RaycastHit hit;
+        //if (LeftTriggerButtonAction != null && LeftTriggerButtonAction.action != null && LeftTriggerButtonAction.action.triggered)
+        //{
+        //    Ray ray = new Ray(rayInteractor.transform.position, rayInteractor.transform.forward);
+        //    RaycastHit hit;
 
-            if (Physics.Raycast(ray, out hit, layerMask))
-            {
-                if (photonView.IsMine || photonView.Owner == null)
-                {
-                    toRotate = hit.collider.gameObject;
-                    isPressed = true;
-                }
-                else
-                {
-                    photonView.RequestOwnership();
-                }
-            }
-        }
+        //    if (Physics.Raycast(ray, out hit, layerMask))
+        //    {
+        //        if (photonView.IsMine || photonView.Owner == null)
+        //        {
+        //            toRotate = hit.collider.gameObject;
+        //            isPressed = true;
+        //        }
+        //        else
+        //        {
+        //            photonView.RequestOwnership();
+        //        }
+        //    }
+        //}
 
         //if (isPressed && RightTriggerButtonAction.action.triggered)
         //{
--- Assets/Scripts/ObjectManipulationXR.cs
+++ Assets/Scripts/ObjectManipulationXR.cs
@@ -1,9 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
+using Photon.Pun;
 
 public class ObjectManipulationXR : MonoBehaviour
 {
+    public InputActionReference triggerButton;
+    public XRRayInteractor rayInteractor;
+    public GameObject RightHandController;
+    public LayerMask layerMask;
+    GameObject grabbedObject;
+    public PhotonView photonView;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +24,58 @@ public class ObjectManipulationXR : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (triggerButton.action.triggered && triggerButton.action != null)
+        {
+            if (photonView.IsMine || photonView.Owner == null)
+            {
+                GrabObject();
+            }
+            else
+            {
+                photonView.RequestOwnership();
+            }
+        }
+        else
+        {
+            ReleaseObject();
+        }
+    }
+
+    public void GrabObject()
+    {
+        Ray ray = new Ray(rayInteractor.transform.position, rayInteractor.transform.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
+        {
+            // Make the grabbed object a child of the controller
+          transform.SetParent(RightHandController.transform);
+        }
+    }
+
+    public void ReleaseObject()
+    {
+        transform.SetParent(null);
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            // This client owns this object: send the others our data
+            stream.SendNext(grabbedObject.transform.position);
+        }
+        else
+        {
+            // Network player, receive data
+            grabbedObject.transform.position = (Vector3)stream.ReceiveNext();
+        }
+    }
+
+    public void OnOwnershipRequest(PhotonView targetView, Photon.Realtime.Player requestingPlayer)
+    {
+        if (targetView != photonView) return;
+
+        // You can add more logic here to decide if you want to transfer ownership
+        photonView.TransferOwnership(requestingPlayer);
     }
 }
