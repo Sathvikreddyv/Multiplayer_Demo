ad368888c2a16460759a5a19b4ced42af24945df
Sathvikreddyv <50298451+Sathvikreddyv@users.noreply.github.com>
2024-05-01 20:01:37 -0400

    photon changes made

---
+++ Assets/PlayerNetworkScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+
+public class PlayerNetworkScript : MonoBehaviourPunCallbacks
+{
+    public GameObject localXRRigObject;
+    public GameObject localInteractionManager;
+    public GameObject localEventSystem;
+    public GameObject inputActionManager;
+
+
+
+    void Start()
+    {
+        if (photonView.IsMine)
+        {
+            localXRRigObject.SetActive(true);
+            localInteractionManager.SetActive(true);
+            localEventSystem.SetActive(true);
+            inputActionManager.SetActive(true);
+
+        }
+        else
+        {
+            localXRRigObject.SetActive(false);
+            localInteractionManager.SetActive(false);
+            localEventSystem.SetActive(false);
+            inputActionManager.SetActive(false);
+        }
+
+    }
+}
