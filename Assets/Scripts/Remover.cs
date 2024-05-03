e82c963889bbd4ffbd432f8a055e0587cca57f79
Sathvikreddyv <50298451+Sathvikreddyv@users.noreply.github.com>
2024-05-03 13:04:49 -0400

    update remove.cs

--- Assets/Scripts/Remover.cs
+++ Assets/Scripts/Remover.cs
@@ -42,7 +42,7 @@ public class Remover : MonoBehaviourPun
         Debug.Log(selectedAsset.name);
         if (selectedAsset.tag != "ground")
         {
-            for(int i = 0; i<20; i++)
+            for(int i = 0; i<21; i++)
             {
                 if(selectedAsset.tag == objectPlacement.button[i].tag)
                 {
